ee5576f432fc3a1ec6c471495fe1ffb70dc1b20e
ErwanR-Holberton <6218@holbertonstudents.com>
2023-12-20 22:57:50 +0100

    revert

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """get data and put it in a csv file"""
-from sys import argv
 import requests
+from sys import argv
 
 if __name__ == "__main__":
     response = requests.get("https://jsonplaceholder.typicode.com/todos",
--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """export to json"""
-from sys import argv
 import json
 import requests
+from sys import argv
 
 if __name__ == "__main__":
     response = requests.get("https://jsonplaceholder.typicode.com/todos",
