d063e81e4ae16f03940c0b12ec872ca6ce3848b0
ErwanR-Holberton <6218@holbertonstudents.com>
2023-12-20 08:50:59 +0100

    removed tests for status

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -9,18 +9,15 @@ if __name__ == "__main__":
     response_user = requests.get("https://jsonplaceholder.typicode.com/users/"
                                  + argv[1])
 
-    if response.status_code == 200 and response_user.status_code == 200:
-        data = response.json()
-        username = response_user.json().get('username')
+    data = response.json()
+    username = response_user.json().get('username')
 
-        formated_data = []
+    formated_data = []
 
-        with open("{}.csv".format(argv[1]), "w") as file:
-            for task in data:
-                id = task.get("userId")
-                completed = task.get("completed")
-                title = task.get("title")
-                file.write('"{}","{}","{}","{}"\n'
-                           .format(id, username, completed, title))
-    else:
-        print(f"Error: {response.status_code}, {response_user.status_code}")
+    with open("{}.csv".format(argv[1]), "w") as file:
+        for task in data:
+            id = task.get("userId")
+            completed = task.get("completed")
+            title = task.get("title")
+            file.write('"{}","{}","{}","{}"\n'
+                        .format(id, username, completed, title))
