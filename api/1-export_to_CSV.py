dce198cdd6a237a3e7e890c8345ba6e9b9f579f1
ErwanR-Holberton <6218@holbertonstudents.com>
2023-12-20 08:36:21 +0100

    use module csv now

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """get data and put it in a csv file"""
+import csv
 import requests
 from sys import argv
 
@@ -11,12 +12,18 @@ if __name__ == "__main__":
 
     if response.status_code == 200 and response_user.status_code == 200:
         data = response.json()
-        username = response_user.json()['username']
+        name = response_user.json()['username']
+
+        formated_data = []
 
         with open("{}.csv".format(argv[1]), "w") as file:
             for task in data:
-                file.write('"{}","{}","{}","{}"\n'
+                row = [task["userId"], name, task["completed"], task["title"]]
+                formated_data.append(row)
+                """file.write('"{}","{}","{}","{}"\n'
                            .format(task["userId"], username, task["completed"],
-                                   task["title"]))
+                                   task["title"]))"""
+            writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+            writer.writerows(formated_data[1:])
     else:
         print(f"Error: {response.status_code}, {response_user.status_code}")
