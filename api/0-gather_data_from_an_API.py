ac193135075fc3d1155380f70b211912bfd66b9d
ErwanR-Holberton <6218@holbertonstudents.com>
2023-12-22 16:52:20 +0100

    fix pycodestyle

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,24 +5,21 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    response = requests.get("https://jsonplaceholder.typicode.com/todos",
-                            params={"userId": argv[1]})
-    response_user = requests.get("https://jsonplaceholder.typicode.com/users/"
-                                 + argv[1])
+    url1 = "https://jsonplaceholder.typicode.com/todos"
+    url2 = "https://jsonplaceholder.typicode.com/users/"
+    response = requests.get(url1, params={"userId": argv[1]})
+    response_user = requests.get(url2 + argv[1])
 
-    if response.status_code == 200 and response_user.status_code == 200:
-        data = response.json()
-        data_user = response_user.json()
-        name = response_user.json().get('name')
-        completed = []
+    data = response.json()
+    data_user = response_user.json()
+    name = response_user.json().get('name')
+    completed = []
 
-        for task in data:
-            if task.get('completed') is True:
-                completed.append(task)
+    for task in data:
+        if task.get('completed') is True:
+            completed.append(task)
 
-        print("Employee {} is done with tasks({}/{}):"
-              .format(name, len(completed), len(data)))
-        for task in completed:
-            print("\t " + task.get('title'))
-    else:
-        print(f"Error: {response.status_code}, {response_user.status_code}")
+    print("Employee {} is done with tasks({}/{}):"
+            .format(name, len(completed), len(data)))
+    for task in completed:
+        print("\t " + task.get('title'))
